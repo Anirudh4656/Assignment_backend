85ff9038285096be900a977b3167eaf2d55cf1ba
ANIRUDH <83304642+Anirudh4656@users.noreply.github.com>
2024-07-17 15:17:04 +0530

    UpdatedverifyApikey.ts

--- app/middlewares/verifyApikey.ts
+++ app/middlewares/verifyApikey.ts
@@ -5,13 +5,6 @@ import createHttpError from "http-errors";
 const verifyApiKey = async (
   req: Request,
   res: Response,
-  next: NextFunction// middleware/verifyApiKey.js
-import { User } from "../schemas/User";
-import { Request, Response, NextFunction } from "express";
-import createHttpError from "http-errors";
-const verifyApiKey = async (
-  req: Request,
-  res: Response,
   next: NextFunction
 ) => {
   const result = req.user;
@@ -35,40 +28,6 @@ const verifyApiKey = async (
     ) {
       return next(
         createHttpError(424, { message: "API Usage limit exceeded." })
-      );
-    }
-
-    req.user = userDetails;
-    next();
-  } catch (error: any) {
-    next(createHttpError(500, { message: error.message }));
-  }
-};
-
-export default verifyApiKey;
-
-) => {
-  const result = req.user;
-  console.log("api key verify", result);
-  if (!result) {
-    throw createHttpError(401, { message: "Unauthorized" });
-  }
-  try {
-    const userDetails = await User.findById(result.id).populate({
-      path: "plan",
-    });
-    if (!userDetails) {
-      throw createHttpError(401, { message: "No details Found" });
-    }
-    if (!userDetails.apiKey) {
-      return next(createHttpError(404, { message: "API key is missing." }));
-    }
-    if (
-      userDetails.plan.length > 0 &&
-      userDetails.apiUsage > userDetails.plan[0].apiLimit
-    ) {
-      return next(
-        createHttpError(429, { message: "API Usage limit exceeded." })
       );
     }
 
