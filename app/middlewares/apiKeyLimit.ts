f6729bc59c22474103ef851d8dda9653d96dfee0
ANIRUDH <83304642+Anirudh4656@users.noreply.github.com>
2024-07-17 14:05:15 +0530

    Updated apiKeyLimit.ts

--- app/middlewares/apiKeyLimit.ts
+++ app/middlewares/apiKeyLimit.ts
@@ -9,14 +9,16 @@ const getRateLimiter = (apiRequestperSecond: number) =>
     message: "Too many requests from this IP, please try again after a second",
     headers: true,
     handler: (req, res, next, options) => {
-      return next(createHttpError(options.statusCode, { message: options.message }));
+      return next(
+        createHttpError(options.statusCode, { message: options.message })
+      );
     },
   });
 
 const apiKeyLimit = async (req: Request, res: Response, next: NextFunction) => {
   try {
     const result = req.user;
-console.log("result  in apikeyLimit",result);
+    console.log("result  in apikeyLimit", result);
     if (!result) {
       throw createHttpError(401, { message: "Unauthorized" });
     }
@@ -41,19 +43,20 @@ console.log("result  in apikeyLimit",result);
         next(err);
       }
     });
-
-    
-   
-  
-    if ( result.storageUsage >=
+    if (result.plan.length === 0) {
+      return next(createHttpError(404, { message: "No plan available." }));
+    }
+    if (
+      result.storageUsage >=
       result?.plan[0].storageLimit * 1024 * 1024 * 1024
     ) {
       throw createHttpError(404, { message: "Memory limit exceeded." });
     }
-    console.log("user is saving",result)
+    console.log("user is saving", result);
+
     result.apiUsage += 1;
     await result.save();
-    console.log("user is saving",result)
+    console.log("user is saving", result);
     next();
   } catch (error: any) {
     next(createHttpError(500, { message: error.message }));
